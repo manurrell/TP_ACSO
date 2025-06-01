3f451550453caf530169cb4c664464a78bae9199
manurrell <borrellmanuel1@gmail.com>
2025-06-01 19:56:07 -0300

    tp4_modificaciones_finales

--- TP4-Shell/src/ej1/ring.c
+++ TP4-Shell/src/ej1/ring.c
@@ -8,17 +8,15 @@
 int main(int argc, char **argv)
 {
     if (argc != 4) {
-        fprintf(stderr, "Uso: anillo <n> <c> <s>\n");
-        exit(1);
+        fprintf(stderr, "Uso: %s <num_processes> <initial_value> <start_index>\n", argv[0]);
+        return 1;
     }
 
     int n = atoi(argv[1]);
     int mensaje_inicial = atoi(argv[2]);
     int start = atoi(argv[3]);
 
-    printf("Se crearán %d procesos, se enviará el caracter %d desde proceso %d\n",
-           n, mensaje_inicial, start);
-    fflush(stdout);
+    printf("%d %d %d\n", n, mensaje_inicial, start);
 
     int pipes[n][2];
     for (int i = 0; i < n; i++) {
@@ -28,17 +26,17 @@ int main(int argc, char **argv)
         }
     }
 
-    // Pipe extra para que el ultimo hijo envíe el resultado al padre
-    int pipe_padre[2];
-    if (pipe(pipe_padre) == -1) {
-        perror("pipe_padre");
+    // Pipe extra
+    int return_pipe[2];
+    if (pipe(return_pipe) == -1) {
+        perror("pipe_r");
         exit(1);
     }
 
     for (int i = 0; i < n; i++) {
         pid_t pid = fork();
         if (pid < 0) {
-            perror("fork");
+            perror("fork error");
             exit(1);
         }
 
@@ -46,30 +44,29 @@ int main(int argc, char **argv)
             int buffer;
             int prev = (i - 1 + n) % n;
 
-            // Cerrar todos los pipes que no usa
+            // cerrar pipes
             for (int j = 0; j < n; j++) {
                 if (j != prev) close(pipes[j][0]);
                 if (j != i) close(pipes[j][1]);
             }
-            close(pipe_padre[0]); 
+            close(return_pipe[0]); 
 
-            // Leer mensaje de proceso anterior
-            ssize_t r = read(pipes[prev][0], &buffer, sizeof(int));
-            if (r != sizeof(int)) {
+
+            ssize_t res = read(pipes[prev][0], &buffer, sizeof(int));
+            if (res != sizeof(int)) {
                 perror("read hijo");
                 exit(1);
             }
-
-            printf("Hijo %d recibió: %d\n", i, buffer);
-            fflush(stdout);
+            // debug print
+            printf("Hijo: %d msj recibido: %d\n", i, buffer);
 
             buffer++; // Incrementar el msj
 
-            // Si es el proceso anterior al start, enviar a padre
+            // Si es el proceso anterior al start (ultimo), enviar a padre
             if (i == (start - 1 + n) % n) {
-                ssize_t w = write(pipe_padre[1], &buffer, sizeof(int));
+                ssize_t w = write(return_pipe[1], &buffer, sizeof(int));
                 if (w != sizeof(int)) {
-                    perror("write pipe_padre");
+                    perror("write return_pipe");
                     exit(1);
                 }
             } else {
@@ -84,19 +81,19 @@ int main(int argc, char **argv)
             // Cerrar pipes usados
             close(pipes[prev][0]);
             close(pipes[i][1]);
-            close(pipe_padre[1]);
+            close(return_pipe[1]);
 
             exit(0);
         }
     }
 
-    // PADRE cierra los extremos de los pipes que no usa
+    // cierro los pipes que no usa
     for (int i = 0; i < n; i++) {
         close(pipes[i][0]);
         if (i != ((start - 1 + n) % n))
             close(pipes[i][1]);
     }
-    close(pipe_padre[1]);
+    close(return_pipe[1]);
 
     int prev = (start - 1 + n) % n;
 
@@ -109,15 +106,14 @@ int main(int argc, char **argv)
     close(pipes[prev][1]);
 
     int resultado;
-    ssize_t r = read(pipe_padre[0], &resultado, sizeof(int));
+    ssize_t r = read(return_pipe[0], &resultado, sizeof(int));
     if (r != sizeof(int)) {
         perror("read padre");
         exit(1);
     }
-    close(pipe_padre[0]);
+    close(return_pipe[0]);
 
     printf("Resultado final recibido por el padre: %d\n", resultado);
-    fflush(stdout);
 
     // Esperar a hijos
     for (int i = 0; i < n; i++){
--- TP4-Shell/src/ej2/shell.c
+++ TP4-Shell/src/ej2/shell.c
@@ -1,117 +1,4 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <unistd.h>
-// #include <sys/wait.h>
-// #include <string.h>
 
-// #define MAX_COMMANDS 200
-// #define MAX_ARGS 100
-
-// int main() {
-//     char command[256];
-//     char *commands[MAX_COMMANDS];
-//     int command_count;
-
-//     while (1) 
-//     {
-//         if (isatty(STDIN_FILENO)){
-//             printf("Shell> ");
-//         }
-        
-//         if (!fgets(command, sizeof(command), stdin)) {
-//             break;
-//         }
-        
-//         command[strcspn(command, "\n")] = '\0';
-
-//         // separor comandos por pipe
-//         command_count = 0;
-//         char *token = strtok(command, "|");
-//         while (token != NULL && command_count < MAX_COMMANDS) 
-//         {
-//             // Quitar espacios al inicio y fin
-//             while (*token == ' ') token++;
-//             char *end = token + strlen(token) - 1;
-//             while (end > token && (*end == ' ')) {
-//                 *end = '\0';
-//                 end--;
-//             }
-
-//             commands[command_count++] = token;
-//             token = strtok(NULL, "|");
-//         }
-
-//         if (command_count == 0) {
-//             continue; // Nada que hacer
-//         }
-
-//         // Pipes: para n comandos, necesitamos n-1 pipes
-//         int pipefd[command_count - 1][2];
-
-//         for (int i = 0; i < command_count - 1; i++) {
-//             if (pipe(pipefd[i]) == -1) {
-//                 perror("pipe");
-//                 exit(EXIT_FAILURE);
-//             }
-//         }
-
-//         for (int i = 0; i < command_count; i++) {
-//             pid_t pid = fork();
-//             if (pid == -1) {
-//                 perror("fork");
-//                 exit(EXIT_FAILURE);
-//             }
-
-//             if (pid == 0) { 
-//                 // Si no es el primer comando, lee del pipe anterior
-//                 if (i > 0) {
-//                     dup2(pipefd[i - 1][0], STDIN_FILENO);
-//                 }
-
-//                 // Si no es el último comando, escribe en el pipe actual
-//                 if (i < command_count - 1) {
-//                     dup2(pipefd[i][1], STDOUT_FILENO);
-//                 }
-
-//                 // Cerrar todos los pipes que no se usan
-//                 for (int j = 0; j < command_count - 1; j++) {
-//                     close(pipefd[j][0]);
-//                     close(pipefd[j][1]);
-//                 }
-
-//                 // Parsear los argumentos del comando i
-//                 char *args[MAX_ARGS];
-//                 int arg_count = 0;
-//                 char *arg = strtok(commands[i], " ");
-//                 while (arg != NULL && arg_count < MAX_ARGS - 1) {
-//                     args[arg_count++] = arg;
-//                     arg = strtok(NULL, " ");
-//                 }
-//                 args[arg_count] = NULL;
-
-//                 // Ejecutar el comando
-//                 if (execvp(args[0], args) == -1) {
-//                     perror("execvp");
-//                     exit(EXIT_FAILURE);
-//                 }
-//             }
-//             // Proceso padre sigue para crear el siguiente hijo
-//         }
-
-//         // Proceso padre: cerrar todos los pipes
-//         for (int i = 0; i < command_count - 1; i++) {
-//             close(pipefd[i][0]);
-//             close(pipefd[i][1]);
-//         }
-
-//         // Esperar a todos los hijos
-//         for (int i = 0; i < command_count; i++) {
-//             wait(NULL);
-//         }
-//     }
-
-//     return 0;
-// }
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -155,9 +42,10 @@ int main() {
     int command_count;
 
     while (1) 
-    {
+    {   
+        // agrego para usar el tester
         if (isatty(STDIN_FILENO)){
-            printf("Shell> ");
+            printf("MyShell> ");
         }
         
         if (!fgets(command, sizeof(command), stdin)) {
@@ -206,7 +94,7 @@ int main() {
                 parse_args(commands[i], args);
 
                 if (execvp(args[0], args) == -1) {
-                    perror("execvp");
+                    perror("execvp error");
                     exit(EXIT_FAILURE);
                 }
             }
